735e74a90a659e3b71ab74bd4d661a904a58c94e
ChrisLinn <nanyishanshaoxiu@gmail.com>
2019-01-01 20:59:09 +0800

    Update settings for hz

--- settings.py
+++ settings.py
@@ -1,8 +1,9 @@
 DBENGINE = 'mysql'  # ENGINE OPTIONS: mysql, sqlite3, postgresql
 DBNAME = 'ershoufang'
-DBUSER = 'root'
-DBPASSWORD = ''
+DBUSER = 'hz-house'
+DBPASSWORD = 'lianjia'
 DBHOST = '127.0.0.1'
 DBPORT = 3306
-CITY = 'bj'  # only one, shanghai=sh shenzhen=sh......
-REGIONLIST = [u'dongcheng']  # only pinyin support
+CITY = 'hz'  # only one, shanghai=sh shenzhen=sh......
+# REGIONLIST = [u'xihu', u'xiacheng', u'jianggan', u'gongshu', u'shangcheng', u'binjiang', u'yuhang', u'xiaoshan', u'tonglu1', u'chunan1', u'jiande', u'fuyang', u'linan', u'xiasha']  # only pinyin support
+REGIONLIST = [u'xihu', u'xiacheng', u'jianggan', u'gongshu', u'shangcheng', u'binjiang']  # only pinyin support
