5e838a357014f0359d0c44d728dce6d1e31f706e
HAOYUatHZ <haoyu@protonmail.com>
2019-02-02 15:29:56 +0800

    classify region

--- settings.py
+++ settings.py
@@ -5,5 +5,7 @@ DBPASSWORD = 'lianjia'
 DBHOST = '127.0.0.1'
 DBPORT = 3306
 CITY = 'hz'  # only one, shanghai=sh shenzhen=sh......
-# REGIONLIST = [u'xihu', u'xiacheng', u'jianggan', u'gongshu', u'shangcheng', u'binjiang', u'yuhang', u'xiaoshan', u'tonglu1', u'chunan1', u'jiande', u'fuyang', u'linan', u'xiasha']  # only pinyin support
-REGIONLIST = [u'xihu', u'xiacheng', u'jianggan', u'gongshu', u'shangcheng', u'binjiang']  # only pinyin support
+
+ALL_REGIONS = [u'xihu', u'xiacheng', u'jianggan', u'gongshu', u'shangcheng', u'binjiang', u'yuhang', u'xiaoshan', u'tonglu1', u'chunan1', u'jiande', u'fuyang', u'linan', u'xiasha']
+PART_REGIONS = [u'xihu', u'xiacheng', u'jianggan', u'gongshu', u'shangcheng', u'binjiang']
+REGIONLIST = PART_REGIONS
